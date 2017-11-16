51ddd25887a9a5b72679d0c9a1b17fd4881c6ace
dennyzhang <denny@dennyzhang.com>
2017-11-16 00:02:51 -0600

    update code

--- utility/bash-scripts/run_chef_solo.sh
+++ utility/bash-scripts/run_chef_solo.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2017-11-15>
-## Updated: Time-stamp: <2017-11-15 23:57:16>
+## Updated: Time-stamp: <2017-11-16 00:02:43>
 ##-------------------------------------------------------------------
 set -e
 
@@ -23,17 +23,39 @@ function log() {
     fi
 }
 
+function get_community_cookbook(){
+    log "get community cookbook"
+    berks_cookbook_folder=${1?}
+    cookbook_folder=${2?}
+    cd "$cookbook_folder"
+    mkdir -p "$berks_cookbook_folder"
+    berks vendor "$berks_cookbook_folder"
+    ls -lth "$berks_cookbook_folder"
+}
+
+function run_chef_solo() {
+    berks_cookbook_folder=${1?}
+    cookbook_folder=${2?}
+    cookbook_name=${3?}
+    cat > solo.rb << EOF
+cookbook_path [File.expand_path(File.join(File.dirname(__FILE__), '..', "cookbooks")), '$berks_cookbook_folder']
+EOF
+    cat > node.json <<EOF
+{
+  "run_list": [ "recipe[$cookbook_name]" ]
+}
+EOF
+    log "Apply chef update"
+    chef-solo -c solo.rb -j node.json
+}
+
 [ -d /home/ec2-user/log ] || mkdir -p /home/ec2-user/log
-LOG_FILE="/home/ec2-user/log/my_docker.log"
+LOG_FILE="/home/ec2-user/log/run_chef_solo.log"
 
 # TODO: customize this
 cookbook_name=${1:-"jenkins-demo-101"}
-
 cookbook_folder="/home/ec2-user/${cookbook_name}"
-berks_cookbook_folder="/tmp/berks_cookbooks"
-mkdir -p "$cookbook_folder"
 
-cd "$cookbook_folder/"
-berks vendor "$berks_cookbook_folder"
-ls -lth "$berks_cookbook_folder"
+get_community_cookbook "/tmp/berks_cookbooks" "$cookbook_folder"
+run_chef_solo "/tmp/berks_cookbooks" "$cookbook_folder" "$cookbook_name"
 ## File: run_chef_solo.sh ends
