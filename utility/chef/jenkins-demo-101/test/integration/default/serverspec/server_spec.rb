c56dd39f93a86f8d44365539d7bf061ac62263d9
dennyzhang <denny@dennyzhang.com>
2017-11-16 00:15:38 -0600

    minor change

--- utility/chef/jenkins-demo-101/test/integration/default/serverspec/server_spec.rb
+++ utility/chef/jenkins-demo-101/test/integration/default/serverspec/server_spec.rb
@@ -9,7 +9,7 @@ set :backend, :exec
 # TODO: make the code more general
 require 'json'
 chef_data = JSON.parse(IO.read('/tmp/kitchen/dna.json'))
-# buildrepo_dir = chef_data.fetch('jenkins_mdm').fetch('buildrepo_dir')
+# BUILDREPO_DIR = CHEF_DATA.FETCH('JENKINS_MDM').FETCH('BUILDREPO_DIR')
 
 describe command('java -version') do
   its(:stderr) { should contain 'java version \"1.8' }
