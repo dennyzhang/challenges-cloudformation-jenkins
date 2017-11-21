e0e33e990bfc6d7f1486acec54f33245753f0621
dennyzhang <denny@dennyzhang.com>
2017-11-20 22:31:32 -0600

    update bash script

--- utility/bash-scripts/run_chef_solo.sh
+++ utility/bash-scripts/run_chef_solo.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2017-11-15>
-## Updated: Time-stamp: <2017-11-19 00:14:58>
+## Updated: Time-stamp: <2017-11-20 22:31:05>
 ##-------------------------------------------------------------------
 set -e
 
@@ -23,10 +23,10 @@ function log() {
     fi
 }
 
-function get_community_cookbook(){
-    log "get community cookbook"
-    berks_cookbook_folder=${1?}
-    cookbook_folder=${2?}
+function get_community_cookbooks(){
+    log "get community cookbooks"
+    local berks_cookbook_folder=${1?}
+    local cookbook_folder=${2?}
     cd "$cookbook_folder"
     mkdir -p "$berks_cookbook_folder"
     berks vendor "$berks_cookbook_folder"
@@ -34,9 +34,11 @@ function get_community_cookbook(){
 }
 
 function run_chef_solo() {
-    berks_cookbook_folder=${1?}
-    cookbook_folder=${2?}
-    cookbook_name=${3?}
+    local berks_cookbook_folder=${1?}
+    local cookbook_folder=${2?}
+    local cookbook_name=${3?}
+    local node_json_file=${4:-""}
+
     working_dir="/home/ec2-user/chef"
     parent_cookbook_folder=$(dirname "$cookbook_folder")
     cd "$working_dir"
@@ -46,14 +48,17 @@ function run_chef_solo() {
 cookbook_path ["$parent_cookbook_folder", "$berks_cookbook_folder"]
 file_cache_path "/var/chef/cache"
 EOF
+    if [ -z "$node_json_file" ]; then
     cat > node.json <<EOF
 {
   "run_list": ["recipe[$cookbook_name]"]
 }
 EOF
+    node_json_file="${working_dir}/node.json"
+    fi
     log "Apply chef-solo update"
     chef-solo --config "${working_dir}/solo.rb" -l debug -L "$LOG_FILE" \
-              --force-formatter --no-color --json-attributes "${working_dir}/node.json"
+              --force-formatter --no-color --json-attributes "$node_json_file"
 }
 
 [ -d /home/ec2-user/log ] || mkdir -p /home/ec2-user/log
@@ -61,8 +66,9 @@ LOG_FILE="/home/ec2-user/log/run_chef_solo.log"
 
 cookbook_name=${1?"cookbook name"}
 cookbook_folder=${2?"cookbook folder"}
+node_json_file=${3:-""}
 berks_cookbooks_folder="/tmp/berks_cookbooks"
 
-get_community_cookbook "$berks_cookbooks_folder" "$cookbook_folder"
-run_chef_solo "$berks_cookbooks_folder" "$cookbook_folder" "$cookbook_name"
+get_community_cookbooks "$berks_cookbooks_folder" "$cookbook_folder"
+run_chef_solo "$berks_cookbooks_folder" "$cookbook_folder" "$cookbook_name" "$node_json_file"
 ## File: run_chef_solo.sh ends
