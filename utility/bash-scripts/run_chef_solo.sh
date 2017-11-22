4b8ac9a023f7729202de9f38407ad15d235352d5
dennyzhang <denny@dennyzhang.com>
2017-11-22 15:31:50 -0600

    avoid verbose output

--- utility/bash-scripts/run_chef_solo.sh
+++ utility/bash-scripts/run_chef_solo.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2017-11-15>
-## Updated: Time-stamp: <2017-11-20 22:31:05>
+## Updated: Time-stamp: <2017-11-22 15:19:58>
 ##-------------------------------------------------------------------
 set -e
 
@@ -57,7 +57,7 @@ EOF
     node_json_file="${working_dir}/node.json"
     fi
     log "Apply chef-solo update"
-    chef-solo --config "${working_dir}/solo.rb" -l debug -L "$LOG_FILE" \
+    chef-solo --config "${working_dir}/solo.rb" -L "$LOG_FILE" \
               --force-formatter --no-color --json-attributes "$node_json_file"
 }
 
